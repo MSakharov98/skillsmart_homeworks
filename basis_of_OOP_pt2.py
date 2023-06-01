53e08250fb5d444385c1d2ab464b539ebadd89e8
Sakharov98 <msaharov669@gmail.com>
2023-06-02 00:33:15 +0300

    Created the composition for Vehicle class

---
+++ basis_of_OOP_pt2.py
@@ -0,0 +1,42 @@
+# 4 Задание
+
+# 4.1 Реализация композиции для кода классов из урока № 4
+
+# a) Для начала преобразуем класс Vehicle
+
+# Создадим класс Engine для управления двигателем
+class Engine:
+    def start(self):
+        print('Engine started')
+
+    def stop(self):
+        print('Engine stopped')
+
+# Далее создадим класс Wheel для управления колесами
+class Wheel:
+    def rotate(self):
+        print('Wheel is rotating')
+
+# Преобразуем класс Vehicle
+class Vehicle:
+    # Добавим машине двигатель и четыре колеса
+    def __init__(self):
+        self.engine = Engine()
+        self.wheels = [Wheel() for _ in range(4)]
+
+    # Реализуем метод начала движения
+    def start(self):
+        self.engine.start()
+        for wheel in self.wheels:
+            wheel.rotate()
+
+    # Реализуем метод остановки
+    def stop(self):
+        self.engine.stop()
+        for wheel in self.wheels:
+            wheel.rotate()
+
+# б) Далее преобразуем класс Fruit
+
+
+
