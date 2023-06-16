61825d2a9120cde1e17018dcb96f171bb00e3983
Sakharov98 <msaharov669@gmail.com>
2023-06-16 19:33:53 +0300

    Finished homerwork 17

---
+++ test_drawing.py
@@ -0,0 +1,48 @@
+from Pillow import Image, ImageDraw, ImageFont
+import glob
+import os
+
+def convert_images(source_extension, target_extension):
+
+    # Находим все файлы с расширением source_extension
+    files = glob.glob(f'*.{source_extension}')
+
+    for file in files:
+        # Открываем изображение
+        image = Image.open(file)
+
+        # Создаем новое имя файла с заменой расширения
+        new_filename = os.path.splitext(file)[0] + f'.{target_extension}'
+
+        # Конвертируем и сохраняем изображение в целевом формате
+        image.save(new_filename, target_extension)
+
+        print(f"Converted {file} to {new_filename}")
+
+        # Рисуем незаполненный квадрат и текст в центре изображения
+        draw = ImageDraw.Draw(image)
+        width, height = image.size
+        square_size = min(width, height) // 2
+
+        square_left = (width - square_size) // 2
+        square_top = (height - square_size) // 2
+        square_right = square_left + square_size
+        square_bottom = square_top + square_size
+
+        draw.rectangle((square_left, square_top, square_right, square_bottom), outline='black')
+
+        text = "Hello,\nWorld!"
+        font = ImageFont.truetype("arial.ttf", 20)
+        text_width, text_height = draw.textsize(text, font=font)
+        text_x = (width - text_width) // 2
+        text_y = (height - text_height) // 2
+        draw.text((text_x, text_y), text, fill='black', font=font)
+
+        # Сохраняем изображение с нарисованным квадратом и текстом
+        image.save(new_filename)
+
+# Пример использования функции
+source_ext = 'jpg'
+target_ext = 'png'
+
+convert_images(source_ext, target_ext)
