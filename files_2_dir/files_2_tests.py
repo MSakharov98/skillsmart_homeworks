b013c8a294c2375d841bf7c1441687b4b9cd704e
Sakharov98 <msaharov669@gmail.com>
2023-06-17 17:17:47 +0300

    Unified tests

--- files_2_dir/files_2_tests.py
+++ files_2_dir/files_2_tests.py
@@ -7,38 +7,41 @@ from files_2 import get_files_with_extension, delete_directory
 class FilesTestCase(unittest.TestCase):
     def setUp(self):
         # Создаем тестовый каталог
-        os.makedirs("/tmp/test_dir")
-        os.makedirs("/tmp/test_dir/subdir")
-        with open("/tmp/test_dir/file1.txt", "w"):
+        self.test_dir = "tmp/test_dir"
+        self.subdir = "subdir"
+        os.makedirs(self.test_dir)
+        os.makedirs(os.path.join(self.test_dir, self.subdir))
+        with open(os.path.join(self.test_dir, "file1.txt"), "w"):
             pass
-        with open("/tmp/test_dir/file2.txt", "w"):
+        with open(os.path.join(self.test_dir, "file2.txt"), "w"):
             pass
-        with open("/tmp/test_dir/subdir/file3.txt", "w"):
+        with open(os.path.join(self.test_dir, self.subdir, "file3.txt"), "w"):
             pass
-        with open("/tmp/test_dir/subdir/file4.txt", "w"):
+        with open(os.path.join(self.test_dir, self.subdir, "file4.txt"), "w"):
             pass
 
     def tearDown(self):
         # Проверяем существование каталога перед удалением
-        if os.path.exists("/tmp/test_dir"):
+        if os.path.exists(self.test_dir):
             # Удаляем тестовый каталог
-            shutil.rmtree("/tmp/test_dir")
+            shutil.rmtree(self.test_dir)
 
     def test_get_files_with_extension(self):
-        result = get_files_with_extension("/tmp/test_dir", ".txt", True)
-        expected_result = (['/tmp/test_dir/file1.txt', '/tmp/test_dir/file2.txt', '/tmp/test_dir/subdir/file3.txt', '/tmp/test_dir/subdir/file4.txt'], [])
+        result = get_files_with_extension(self.test_dir, ".txt", True)
+        expected_result = (['tmp/test_dir/file1.txt', 'tmp/test_dir/file2.txt', 'tmp/test_dir/subdir/file3.txt', 'tmp/test_dir/subdir/file4.txt'], [])
         self.assertEqual(result, expected_result)
 
     def test_delete_directory(self):
         # Проверяем существование каталога перед удалением
-        self.assertTrue(os.path.exists("/tmp/test_dir"))
+        self.assertTrue(os.path.exists(self.test_dir))
 
-        success = delete_directory("/tmp/test_dir")
+        success = delete_directory(self.test_dir)
         self.assertTrue(success)  # Проверяем успешное удаление каталога
-        self.assertFalse(os.path.exists("/tmp/test_dir"))  # Проверяем, что каталог был удален
+        self.assertFalse(os.path.exists(self.test_dir))  # Проверяем, что каталог был удален
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
