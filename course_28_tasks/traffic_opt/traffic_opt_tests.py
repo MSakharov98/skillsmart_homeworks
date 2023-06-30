4a3e78970ddd5bc7e9fd8daa54cbcb23cdfd28e8
Sakharov98 <msaharov669@gmail.com>
2023-06-30 16:19:28 +0300

    Added more tests

--- course_28_tasks/traffic_opt/traffic_opt_tests.py
+++ course_28_tasks/traffic_opt/traffic_opt_tests.py
@@ -1,9 +1,16 @@
 import unittest
-
+from traffic_opt import Unmanned
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_study_case1(self):
+        self.assertEqual(Unmanned(10, 2, [[3,5,5], [5,2,2]]), 12)
+
+    def test_study_case2(self):
+        self.assertEqual(Unmanned(10, 2, [[11, 5, 5], [15, 2, 2]]), 10)
+
+    def test_case(self):
+        self.assertEqual(Unmanned(15, 3, [[8,3,4], [12,2,2], [18,3,4]]), 17)
+
 
 
 if __name__ == '__main__':
