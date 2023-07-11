b880f905567750a1edc3ceee5a7ee485589ba9c9
Sakharov98 <msaharov669@gmail.com>
2023-07-11 19:13:08 +0300

    Made code easier

--- Recursion/power/power.py
+++ Recursion/power/power.py
@@ -1,7 +1,10 @@
 def power(N, M):
+
     if M == 0:
         return 1
-    elif M < 0:
-        return 1 / power(N, -M)
-    else:
-        return N * power(N, M - 1)
+
+    if M < 0:
+        N = 1 / N
+        M = -M
+
+    return N * power(N, M - 1)
