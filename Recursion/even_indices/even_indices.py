013e03080b9b683e624e8a679ee708d7ae154412
Sakharov98 <msaharov669@gmail.com>
2023-07-12 14:39:10 +0300

    Corrected solution

--- Recursion/even_indices/even_indices.py
+++ Recursion/even_indices/even_indices.py
@@ -1,17 +1,16 @@
-def get_even_indices(lst):
+def even_indices(lst, index=None):
+
+    if index is None:
+        index = 0
+
+    if index >= len(lst):
+        return []
+
     result = []
 
-    def helper(index=0):
-        nonlocal result
+    if index % 2 == 0 and index != 0:
+        result.append(lst[index])
 
-        if index >= len(lst):
-            return
+    return result + even_indices(lst, index + 1)
 
-        if index % 2 == 0 and index != 0:
-            result.append(lst[index])
 
-        helper(index + 1)
-
-    helper()
-
-    return result
--- Recursion/even_indices/test_even_indices.py
+++ Recursion/even_indices/test_even_indices.py
@@ -1,13 +1,13 @@
 import unittest
-from even_indices import get_even_indices
+from even_indices import even_indices
 
 class MyTestCase(unittest.TestCase):
 
     def test_regular(self):
-        self.assertEqual(get_even_indices([1, 2, 3, 4, 5, 6]), [3, 5])
+        self.assertEqual(even_indices([1, 2, 3, 4, 5, 6]), [3, 5])
 
     def test_empty(self):
-        self.assertEqual(get_even_indices([]), [])
+        self.assertEqual(even_indices([]), [])
 
 
 if __name__ == '__main__':
