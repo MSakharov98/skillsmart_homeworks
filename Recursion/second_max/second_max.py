cdb90db36c3367328538a5e5729e0d897d4c5a64
Sakharov98 <msaharov669@gmail.com>
2023-07-12 17:57:38 +0300

    Corrected code

--- Recursion/second_max/second_max.py
+++ Recursion/second_max/second_max.py
@@ -4,10 +4,11 @@ def find_max(arr):
 
     if len(arr) == 1:
         return arr[0]
-    else:
-        return max(arr[0], find_max(arr[1:]))
+
+    return max(arr[0], find_max(arr[1:]))
 
 def find_second_max(arr):
+
     if not arr:
         return None
 
@@ -15,3 +16,4 @@ def find_second_max(arr):
     arr.remove(max_value)
 
     return find_max(arr)
+
