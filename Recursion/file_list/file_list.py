7ac06d625475a31529625d72c270ac785affacf3
Sakharov98 <msaharov669@gmail.com>
2023-07-13 21:56:21 +0300

    Corrected code of searching files

--- Recursion/file_list/file_list.py
+++ Recursion/file_list/file_list.py
@@ -1,19 +1,18 @@
 import os
-
 def find_files(directory):
-    files_list = []
-    search_files_recursive(directory, files_list)
-
+    files_list = search_files_recursive(directory)
     return files_list
 
-def search_files_recursive(directory, files_list):
+def search_files_recursive(directory):
+    files_list = []
 
     for root, dirs, files in os.walk(directory):
-
         for file in files:
             file_path = os.path.join(root, file)
             files_list.append(file_path)
 
         for dir in dirs:
             dir_path = os.path.join(root, dir)
-            search_files_recursive(dir_path, files_list)
+            files_list.extend(search_files_recursive(dir_path))
+
+    return files_list
