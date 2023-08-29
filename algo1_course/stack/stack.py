a2288fe96cc0a6732e4b39015ff6c522341073b0
Sakharov98 <msaharov669@gmail.com>
2023-08-29 17:04:53 +0300

    Finished stack task

---
+++ algo1_course/stack/stack.py
@@ -0,0 +1,21 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        if self.size() != 0:
+            x = self.stack[-1]
+            self.stack.pop()
+            return x
+        return None
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def peek(self):
+        if self.size() != 0:
+            return self.stack[-1]
+        return None
