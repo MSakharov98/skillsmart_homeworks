d9457dd1939eddf05473cd82fca7346034f4817a
Sakharov98 <msaharov669@gmail.com>
2023-11-27 12:15:33 +0300

    Finished lecture about vars name and its homework

---
+++ clean_code/variables_name.py
@@ -0,0 +1,40 @@
+'''
+Данные переменные взяты из примеров различных решений задач, в том числе на Code Wars и LeetCode, а также
+среди ранее выполненных домашних заданий.
+
+1. old_age - user_current_age_in_years (user_current_age_in_years)
+// текущий возраст пользователя в годах
+
+2. radius_of_circle - calculated_circle_area_radius (calculated_circle_area_radius)
+// радиус круга для вычисления площади
+
+3. current_temperature_celsius - recorded_temperature_in_celsius (recorded_temperature_in_celsius)
+// зарегистрированная температура в градусах Цельсия
+
+4. total_sales_amount_result - cumulative_sales_amount (cumulative_sales_amount)
+// накопленная сумма продаж
+
+5. student_grades_data - grades_of_registered_students (grades_of_registered_students)
+// оценки зарегистрированных студентов
+
+6. car_current_speed - recorded_speed_of_vehicle (recorded_speed_of_vehicle)
+// зарегистрированная скорость автомобиля
+
+7. product_description_item - detailed_product_information (detailed_product_information)
+// подробная информация о продукте
+
+8. account_balance_amount - current_balance_in_account (current_balance_in_account)
+// текущий баланс на счете
+
+9. customer_information_data - detailed_customer_profile (detailed_customer_profile)
+// подробная информация о клиенте
+
+10. stock_price_value - real-time_stock_market_price (real_time_stock_market_price)
+// текущая цена на фондовом рынке
+
+11. total_items_count_result - aggregated_number_of_listed_items (aggregated_number_of_listed_items)
+// общее количество перечисленных элементов
+
+12. calculation_result_res - final_result_of_complex_calculation (final_result_of_complex_calculation)
+// окончательный результат сложного вычисления
+'''
