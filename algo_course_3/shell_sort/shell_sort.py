3852b5ffa69fb24c79759a85a276ab333aba9460
Mikhail Sakharov <msaharov669@gmail.com>
2024-04-24 00:02:11 +0300

    Finished lecture about Shell sort

---
+++ algo_course_3/shell_sort/shell_sort.py
@@ -0,0 +1,16 @@
+def InsertionSortStep(array, step, i):
+    for s in range(i + step, len(array), step):
+        for j in range(s, i + step - 1, -step):
+            if array[j] < array[j - step]:
+                array[j], array[j - step] = array[j - step], array[j]
+
+
+def KnuthSequence(array_size, k_seq=None, i=1):
+    if k_seq is None:
+        k_seq = []
+    if not array_size:
+        return [1]
+    if array_size >= i:
+        KnuthSequence(array_size, k_seq, 3 * i + 1)
+        k_seq.append(i)
+    return k_seq
