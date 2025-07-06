5e4cc19da73a95cc7eea81eebb463cf53616a834
MSakharov98 <134206137+MSakharov98@users.noreply.github.com>
2025-07-06 19:37:28 +0300

    Add files via upload

---
+++ ooap2/task22.py
@@ -0,0 +1,11 @@
+class Animal:
+    def move(self):
+        return "moves in some way"
+
+class Bird(Animal):
+    def move(self):
+        return "flies"
+
+class Fish(Animal):
+    def move(self):
+        return "swims"
